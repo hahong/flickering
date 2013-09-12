48294216b0ea0638c01d65e6033e3f030f3804e6
Ha Hong <hahong84@gmail.com>
2013-09-11 20:10:52 -0400

    misc

--- src/setup.py
+++ src/setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy
 
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("ffast", ["ffast.pyx"], libraries=["m"])]
+    ext_modules = [Extension("ffast", ["ffast.pyx"], libraries=["m"], include_dirs=[numpy.get_include()])]
 )
 
